14228538ad3e93b1318645c9089c5890097b3c20
Lia Ossanna <lossanna@email.arizona.edu>
2022-05-09 22:20:37 -0700

    Compile 2012-2021 differences for cover and diversity to use for SEM

---
+++ scripts/Difference_2012-2021.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+
+# Load data ---------------------------------------------------------------
+
+ground.all <- read.csv("data/cleaned/Summarised-all_ground-cover.csv")
+total.all <- read.csv("data/cleaned/Summarised-all_total-plant-cover.csv")
+fungr.all <- read.csv("data/cleaned/Summarised-all_functional-group-cover.csv")
+gfst.all <- read.csv("data/cleaned/Summarised-all_grass-forb-shrub-tree-cover.csv")
+woody.all <- read.csv("data/cleaned/Summarised-all_woody-herb-cover.csv")
+inwood.all <- read.csv("data/cleaned/Summarised-all_invasive-woody-cover.csv")
+ingfst.all <- read.csv("data/cleaned/Summarised-all_invasive-grassforbshrubtree-cover.csv")
+innat.all <- read.csv("data/cleaned/Summarised-all_invasive-native-cover.csv")
+per.diversity <- read.csv("data/cleaned/All-Nov_perennial-diversity.csv")
+
+
+# Find difference in cover from 2012 to 2021 ------------------------------
+
+soil.wide <- ground.all %>% 
+  filter(Year != "2012-03-01") %>% 
+  filter(Common == "Soil") %>% 
+  pivot_wider(names_from = Year, values_from = Cover) %>% 
+  mutate(soil.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+total.wide <- total.all %>% 
+  filter(Year != "2012-03-01") %>% 
+  pivot_wider(names_from = Year, values_from = Cover) %>% 
+  mutate(total.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+richness.wide <- per.diversity %>% 
+  select(-shan) %>% 
+  pivot_wider(names_from = Year, values_from = rich) %>% 
+  mutate(rich.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+shannon.wide <- per.diversity %>% 
+  select(-rich) %>% 
+  pivot_wider(names_from = Year, values_from = shan) %>% 
+  mutate(shan.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+herb.wide <- woody.all %>% 
+  filter(Year != "2012-03-01",
+         woody == "Herbaceous") %>% 
+  pivot_wider(names_from = Year, values_from = Cover) %>% 
+  mutate(herb.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+wood.wide <- woody.all %>% 
+  filter(Year != "2012-03-01",
+         woody == "Woody") %>% 
+  pivot_wider(names_from = Year, values_from = Cover) %>% 
+  replace(is.na(.), 0) %>% # NA indicates woody plants were not measured that year; hence, 0
+  mutate(wood.diff = `2021-11-01` - `2012-11-01`) %>% 
+  select(-`2012-11-01`, -`2013-11-01`, -`2014-11-01`, 
+         -`2015-11-01`, -`2018-11-01`, -`2021-11-01`)
+
+
+# Combine -----------------------------------------------------------------
+
+diff.12.21 <- total.wide %>% 
+  left_join(richness.wide) %>% 
+  left_join(shannon.wide) %>% 
+  left_join(herb.wide) %>% 
+  select(-woody) %>% 
+  left_join(wood.wide) %>% 
+  filter(Station != "Station 02 + 04") # Nov 2012 data sheet is missing for this one
+
+
+# Save --------------------------------------------------------------------
+
+write.csv(diff.12.21,
+          file = "data/cleaned/Difference_2012-2021.csv")
+
+save.image("RData-RMarkdown/Difference_2012-2021.RData")
