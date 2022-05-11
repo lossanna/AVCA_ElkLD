700e99e3005bebb82a0eaa92be866c271b52638a
Lia Ossanna <lossanna@email.arizona.edu>
2022-05-11 16:10:57 -0700

    Add TN to SEM and model richness

--- scripts/Exploratory-SEM.R
+++ scripts/Exploratory-SEM.R
@@ -9,7 +9,7 @@ dat.2021$inch.trt <- as.factor(dat.2021$inch.trt)
 dat.2021$up.trt <- as.factor(dat.2021$up.trt)
 
 
-####### SEM (no latent variables) #########################################
+# Model number does not mean variables are preserved across all models of same number
 
 
 # Total cover -------------------------------------------------------------
@@ -63,6 +63,34 @@ summary(fit.total3, standardized = TRUE)
 semPaths(fit.total3, "std", edge.label.cex = 0.5, curvePivot = TRUE, residuals = FALSE)
 
 
+# Model 4 (cover 2021)
+mod.total4 <- '
+  # regressions
+    Cover ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.total4 <- sem(mod.total4, data = dat.2021, fixed.x = TRUE) # got warning about variance
+vartable(fit.total4)
+summary(fit.total4, standardized = TRUE)
+
+semPaths(fit.total4, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+# Model 5 (difference 2021)
+mod.total5 <- '
+  # regressions
+    total.diff ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.total5 <- sem(mod.total5, data = dat.2021, fixed.x = TRUE) # got warning about variance
+vartable(fit.total5)
+summary(fit.total5, standardized = TRUE)
+
+semPaths(fit.total5, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
 
 # Herbaceous --------------------------------------------------------------
 
@@ -95,7 +123,7 @@ fit.herb2 <- sem(mod.herb2, data = dat.2021, fixed.x = TRUE)
 varTable(fit.herb2)
 summary(fit.herb2, standardized = TRUE)
 
-semPaths(fit.herb2, "std", edge.label.cex = 0.5, curvePivot = TRUE, residuals = FALSE)
+semPaths(fit.herb2, "std", edge.label.cex = 0.5, residuals = FALSE)
 
 
 # Model 3 (difference)
@@ -121,26 +149,124 @@ fit.herb4 <- sem(mod.herb4, data = dat.2021, fixed.x = TRUE) # got warning about
 varTable(fit.herb4)
 summary(fit.herb4, standardized = TRUE)
 
-tiff("output_figs/Prelim-SEM_herb-cover_width_up-in-trt_elevation.tiff",
-     units = "in", height = 4, width = 6, res = 150)
-semPaths(fit.herb4, "std", edge.label.cex = 1, residuals = FALSE,
-         sizeMan = 7)
-dev.off()
+semPaths(fit.herb4, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+# Model 5 (difference 2021)
+mod.herb5 <- '
+  # regressions
+    herb.diff ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.herb5 <- sem(mod.herb5, data = dat.2021, fixed.x = TRUE) # got warning about variance
+vartable(fit.herb5)
+summary(fit.herb5, standardized = TRUE)
+
+semPaths(fit.herb5, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+
+# Woody -------------------------------------------------------------------
+
+# Model 1 (cover 2021)
+mod.wood1 <- '
+  # regressions
+    Woody ~ Width + up.trt + inch.trt + Elev_Diff + TN_log
+    TN_log ~ up.trt + inch.trt
+'
+fit.wood1 <- sem(mod.wood1, data = dat.2021, fixed.x = TRUE) # got warning about variance
+varTable(fit.wood1)
+summary(fit.wood1, standardized = TRUE)
+
+semPaths(fit.wood1, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+# Model 2 (difference 2021)
+mod.wood2 <- '
+  # regressions
+    wood.diff ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.wood2 <- sem(mod.wood2, data = dat.2021, fixed.x = TRUE) # got warning about variance
+vartable(fit.wood2)
+summary(fit.wood2, standardized = TRUE)
+
+semPaths(fit.wood2, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+
+# Richness ----------------------------------------------------------------
+
+# Model 1 (richness 2021)
+mod.rich1 <- '
+  # regressions
+    rich ~ Width + up.trt + inch.trt + Elev_Diff + TN_log
+    TN_log ~ up.trt + inch.trt
+'
+fit.rich1 <- sem(mod.rich1, data = dat.2021, fixed.x = TRUE) 
+varTable(fit.rich1)
+summary(fit.rich1, standardized = TRUE)
+
+semPaths(fit.rich1, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+# Model 2 (difference 2021)
+mod.rich2 <- '
+  # regressions
+    rich.diff ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.rich2 <- sem(mod.rich2, data = dat.2021, fixed.x = TRUE)
+vartable(fit.rich2)
+summary(fit.rich2, standardized = TRUE)
+
+semPaths(fit.rich2, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
 
 
 # Shannon diversity -------------------------------------------------------
 
 # Model 1 (Shannon 2021)
-mod.shannon1 <- '
+mod.shan1 <- '
   # regressions
     shan ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc
     OM_perc ~ up.trt + inch.trt
 '
-fit.shannon1 <- sem(mod.shannon1, data = dat.2021, fixed.x = TRUE)
-varTable(fit.shannon1)
-summary(fit.shannon1, standardized = TRUE)
+fit.shan1 <- sem(mod.shan1, data = dat.2021, fixed.x = TRUE)
+varTable(fit.shan1)
+summary(fit.shan1, standardized = TRUE)
 
-semPaths(fit.shannon1, "std", residuals = FALSE)
+semPaths(fit.shan1, "std", residuals = FALSE)
+
+# Model 2 (Shannon 2021)
+mod.shan2 <- '
+  # regressions
+    shan ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.shan2 <- sem(mod.shan2, data = dat.2021, fixed.x = TRUE) 
+vartable(fit.shan2)
+summary(fit.shan2, standardized = TRUE)
+
+semPaths(fit.shan2, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
+
+
+# Model 2 (Shannon 2021)
+mod.shan3 <- '
+  # regressions
+    shan.diff ~ Width + up.trt + inch.trt + Elev_Diff + OM_perc + TN_log
+    OM_perc ~ up.trt + inch.trt
+    TN_log ~ up.trt + inch.trt
+'
+fit.shan3 <- sem(mod.shan3, data = dat.2021, fixed.x = TRUE) 
+vartable(fit.shan3)
+summary(fit.shan3, standardized = TRUE)
+
+semPaths(fit.shan3, "std", edge.label.cex = 1.3, residuals = FALSE, sizeMan = 7)
 
 
 # Save --------------------------------------------------------------------
