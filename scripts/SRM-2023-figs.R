6d2712a2664e5cfe4db540a5672cef7506ef67ba
Lia Ossanna <lossanna@arizona.edu>
2023-02-01 22:54:00 -0700

    Make figs from veg monitoring data

--- scripts/SRM-2023-figs.R
+++ scripts/SRM-2023-figs.R
@@ -59,7 +59,7 @@ total.plot.srm23 <- ggplot(total.channel, aes(x = year.xaxis, y = mean,
             size = 3.5, color = "gray30")
 total.plot.srm23
 
-tiff("output_figs/SRM_2023/Total plant cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
+tiff("output_figs/SRM_2023/Total-plant-cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
 total.plot.srm23
 dev.off()
 
@@ -123,7 +123,7 @@ herb.plot.srm23 <- ggplot(herb.channel, aes(x = year.xaxis, y = mean,
             size = 3, color = "gray30")
 herb.plot.srm23
 
-tiff("output_figs/SRM_2023/Herbaceous cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
+tiff("output_figs/SRM_2023/Herbaceous-cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
 herb.plot.srm23
 dev.off()
 
@@ -131,74 +131,145 @@ dev.off()
 
 # Invasive/native ---------------------------------------------------------
 
+letters <- data.frame(label = c(native13.letters$groups,
+                                native19.letters$groups,
+                                invas19.letters$groups.up),
+                      channel.trt = c(rep("Channel 13: In-channel treatment", 6),
+                                      rep("Channel 19: Upland treatment", 12)),
+                      x = rep(total.channel$year.xaxis[1:6], 3),
+                      y = c(63, 60, 64, 60, 60, 42,
+                            70, 68, 68, 37, 50, 60,
+                            10, 10, 10, 10, 15, 20))
 
-# Invasive/native and woody/herbaceous ------------------------------------
+anova.lab <- data.frame(label = c(rep("ANOVA", 2)),
+                        channel.trt = c("Channel 13: In-channel treatment",
+                                        "Channel 19: Upland treatment"),
+                        x = c(rep(as.Date("2020-01-01"), 2)),
+                        y = c(75, 75))
 
-inwood.known.plot.srm23 <- ggplot(inwood.channel.known.nov, aes(x = Year, y = mean, 
-                                                              group = inwood, color = inwood, shape = inwood)) +
-  geom_line(linewidth = 1) +
-  geom_point(size = 3) +
-  geom_errorbar(aes(ymin = mean - SE, ymax = mean + SE), 
-                position = position_dodge(0.05)) +
+innat.known.plot.srm23 <- ggplot(innat.channel.known, aes(x = year.xaxis, y = mean)) +
+  geom_line(linewidth = 1, aes(color = Native)) +
+  geom_point(size = 3, aes(color = Native, shape = Native)) +
+  geom_pointrange(aes(ymin = mean - SE, ymax = mean + SE, color = Native, shape = Native)) +
   scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
-  facet_wrap(~channel.trt) +
   xlab(NULL) +
   ylab("Cover (%)") +
-  ggtitle("Woody and herbaceous cover") +
+  ggtitle("Plant cover by native status") +
   scale_color_brewer(palette = "Dark2") +
-  theme_bw(base_size = 18) +
+  theme_bw(base_size = 13) +
   theme(legend.title = element_blank()) +
-  theme(legend.position = "bottom")
-inwood.known.plot.srm23
+  theme(legend.position = "bottom") +
+  facet_wrap(~channel.trt) +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label, group = channel.trt),
+            color = "black") +
+  geom_text(data = anova.lab,
+            mapping = aes(x = x, y = y, label = label, group = channel.trt),
+            size = 3.5, color = "gray30")
+innat.known.plot.srm23
 
-tiff("output_figs/SRM_2023/Inwood cover.tiff", units = "in", height = 7, width = 10, res = 300)
-inwood.known.plot.srm23
+tiff("output_figs/SRM_2023/Invasive-native-cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
+innat.known.plot.srm23
 dev.off()
 
-# Invasive herb
-# KW for all
-# NS for C12, C13, C21
-ivherb19.letters
 
-# Native herb
-hsd.ntherb12
-ntherb13.letters
-ntherb19.letters
-# C21 KW NS
+# Invasive/native for herbaceous ------------------------------------------
+
+letters <- data.frame(label = c(ntherb13.letters$groups,
+                                ntherb19.letters$groups,
+                                ivherb19.letters$groups.up),
+                      channel.trt = c(rep("Channel 13: In-channel treatment", 6),
+                                      rep("Channel 19: Upland treatment", 12)),
+                      x = rep(total.channel$year.xaxis[1:6], 3),
+                      y = c(13, 10, 15, 13, 22, 17,
+                            23, 16, 27, 13, 17, 20,
+                            2, 2, 2, 2, 3, 8))
+
+anova.lab <- data.frame(label = c(rep("ANOVA", 2)),
+                        channel.trt = c("Channel 13: In-channel treatment",
+                                        "Channel 19: Upland treatment"),
+                        x = c(rep(as.Date("2020-01-01"), 2)),
+                        y = c(28, 28))
+
+inwood.known.herb.plot.srm23 <- ggplot(inwood.channel.known.herb, 
+                                         aes(x = year.xaxis, y = mean)) +
+  geom_line(linewidth = 1, aes(color = inwood)) +
+  geom_point(size = 3, aes(color = inwood, shape = inwood)) +
+  geom_pointrange(aes(ymin = mean - SE, ymax = mean + SE, color = inwood, shape = inwood)) +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  xlab(NULL) +
+  ylab("Cover (%)") +
+  ggtitle("Herbaceous cover by native status") +
+  scale_color_brewer(palette = "Dark2") +
+  theme_bw(base_size = 13) +
+  theme(legend.title = element_blank()) +
+  theme(legend.position = "bottom") +
+  facet_wrap(~channel.trt) +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label, group = channel.trt),
+            color = "black") +
+  geom_text(data = anova.lab,
+            mapping = aes(x = x, y = y, label = label, group = channel.trt),
+            size = 3.5, color = "gray30")
+inwood.known.herb.plot.srm23
+
+tiff("output_figs/SRM_2023/Invasive-native-cover_herbaceous.tiff", units = "in", height = 5.5, width = 10, res = 300)
+inwood.known.herb.plot.srm23
+dev.off()
 
 
 # Species richness --------------------------------------------------------
 
-richness.plot.srm23 <- ggplot(richness.channel.year, aes(x = Year, y = mean, 
-                                                      group = channel.trt, color = channel.trt)) +
+letters <- data.frame(label = c(richness13.letters$groups,
+                                                    richness19.letters$groups,
+                                                    richness21.letters$groups),
+                                          channel.trt = c(rep("Channel 13: In-channel treatment", 6),
+                                                          rep("Channel 19: Upland treatment", 6),
+                                                          rep("Channel 21: In-channel treatment", 6)),
+                                          x = rep(richness.channel$year.xaxis[1:6], 3),
+                                          y = c(9.3, 8.2, 8.2, 9, 10.6, 9,
+                                                10.9, 9.5, 9.5, 10.1, 8.2, 8.1,
+                                                7.6, 7.9, 9.8, 8.5, 9.7, 6.9))
+
+anova.lab <- data.frame(label = c(rep("ANOVA", 3)),
+                        channel.trt = c("Channel 13: In-channel treatment", 
+                                        "Channel 19: Upland treatment",
+                                        "Channel 21: In-channel treatment"),
+                        x = c(rep(as.Date("2020-01-01"), 3)),
+                        y = c(10.5, 10.5, 10.5))
+
+richness.plot.srm23 <- ggplot(richness.channel, aes(x = year.xaxis, y = mean, 
+                                                      group = channel.trt, 
+                                                      color = channel.trt)) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
   geom_pointrange(aes(ymin = mean - SE, ymax = mean + SE)) +
-  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   xlab(NULL) +
   ylab("Number of species") +
   ggtitle("Perennial species richness") +
   scale_color_manual(values = c("red", "#33A02C", "#1F78B4", "#33A02C")) +
-  theme_bw(base_size = 18) +
+  theme_bw(base_size = 14) +
   theme(legend.position = "none") +
-  facet_wrap(~channel.trt)
+  facet_wrap(~channel.trt) +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  geom_text(data = anova.lab,
+            mapping = aes(x = x, y = y, label = label),
+            size = 3, color = "gray30")
 richness.plot.srm23
 
 tiff("output_figs/SRM_2023/Richess.tiff", units = "in", height = 5.5, width = 10, res = 300)
 richness.plot.srm23
 dev.off()
 
-hsd.richness12
-hsd.richness13
-richness19.letters
-richness21.letters
-
 
 # Shannon diversity -------------------------------------------------------
 
-
-shannon.plot.srm23 <- ggplot(shannon.channel.year, aes(x = Year, y = mean, 
-                                                    group = channel.trt, color = channel.trt)) +
+shannon.plot.srm23 <- ggplot(shannon.channel, aes(x = year.xaxis, y = mean, 
+                                            group = channel.trt, 
+                                            color = channel.trt)) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
   geom_pointrange(aes(ymin = mean - SE, ymax = mean + SE)) +
@@ -207,7 +278,7 @@ shannon.plot.srm23 <- ggplot(shannon.channel.year, aes(x = Year, y = mean,
   ylab("Shannon diversity index") +
   ggtitle("Perennial plant diversity") +
   scale_color_manual(values = c("red", "#33A02C", "#1F78B4", "#33A02C")) +
-  theme_bw(base_size = 18) +
+  theme_bw(base_size = 14) +
   theme(legend.position = "none") +
   facet_wrap(~channel.trt)
 shannon.plot.srm23
@@ -216,14 +287,35 @@ tiff("output_figs/SRM_2023/Shannon.tiff", units = "in", height = 5.5, width = 10
 shannon.plot.srm23
 dev.off()
 
-# NS for all
-# KW for C19; ANOVA for all others
 
 
 # Soil cover --------------------------------------------------------------
 
-ground.soil.plot.srm23 <- ggplot(ground.channel.nov.soil, aes(x = Year, y = mean, 
-                                                              group = channel.trt, color = channel.trt)) +
+letters <- data.frame(label = c(soil12.letters$groups, 
+                                soil13.letters$groups,
+                                soil19.letters$groups,
+                                soil21.letters$groups),
+                      channel.trt = c(rep("Channel 12: No treatment", 6),
+                                      rep("Channel 13: In-channel treatment", 6),
+                                      rep("Channel 19: Upland treatment", 6),
+                                      rep("Channel 21: In-channel treatment", 6)),
+                      x = rep(total.channel$year.xaxis[1:6], 4),
+                      y = c(40, 52, 42, 60, 40, 53,
+                            65, 61, 57, 40, 32, 35,
+                            50, 55, 40, 63, 50, 40,
+                            53, 59, 50, 47, 53, 25))
+
+anova.lab <- data.frame(label = c(rep("ANOVA", 4)),
+                        channel.trt = c("Channel 12: No treatment",
+                                        "Channel 13: In-channel treatment",
+                                        "Channel 19: Upland treatment",
+                                        "Channel 21: In-channel treatment"),
+                        x = c(rep(as.Date("2020-01-01"), 4)),
+                        y = c(65, 65, 65, 65))
+
+ground.soil.plot.srm23 <- ggplot(ground.channel.soil, 
+                                   aes(x = year.xaxis, y = mean, 
+                                       group = channel.trt, color = channel.trt)) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
   geom_pointrange(aes(ymin = mean - SE, ymax = mean + SE)) +
@@ -233,15 +325,19 @@ ground.soil.plot.srm23 <- ggplot(ground.channel.nov.soil, aes(x = Year, y = mean
   ylab("Cover (%)") +
   ggtitle("Soil cover") +
   scale_color_manual(values = c("red", "#33A02C", "#1F78B4", "#33A02C")) +
-  theme_bw(base_size = 18) +
-  theme(legend.position = "none")
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  geom_text(data = anova.lab,
+            mapping = aes(x = x, y = y, label = label),
+            size = 3, color = "gray30")
 ground.soil.plot.srm23
 
-tiff("output_figs/SRM_2023/Soil cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
+tiff("output_figs/SRM_2023/Soil-cover.tiff", units = "in", height = 5.5, width = 10, res = 300)
 ground.soil.plot.srm23
 dev.off()
 
-soil12.letters
-hsd.soil13
-hsd.soil19
-soil21.letters
+
+save.image("RData/SRM-2023-figs.RData")
