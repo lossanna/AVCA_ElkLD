84627ffffde534a6476d5f61ef13482764213f2f
Lia Ossanna <lossanna@arizona.edu>
2023-02-02 21:15:18 -0700

    Graph precip on its own

---
+++ scripts/PimaCounty_precip.R
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(plotrix)
+
+# Load data ---------------------------------------------------------------
+
+precip <- read.table("data/PimaCounty_precip/PimaCounty_precip_2012-2021.txt",
+                     sep = "\t", header = TRUE)
+
+
+# Data wrangling ----------------------------------------------------------
+
+precip$year.xaxis <- as.Date(precip$year.xaxis)
+
+
+# Graph -------------------------------------------------------------------
+
+ggplot(precip, aes(x = year.xaxis, y = Precip_cum)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3) +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  xlab(NULL) +
+  ylab("Precipitation (in)") +
+  ggtitle("Cumulative summer precipitation") +
+  theme_bw() +
+  scale_y_continuous(limits = c(0, 14))
+  
+
