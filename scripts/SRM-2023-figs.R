90aecbe58f9c02bf210454d8bdc7e4fd6bfadc42
Lia Ossanna <lossanna@arizona.edu>
2023-02-03 01:18:15 -0700

    Update SRM 2023 figs to use treatment2 for 2021 comparisons

--- scripts/SRM-2023-figs.R
+++ scripts/SRM-2023-figs.R
@@ -8,7 +8,7 @@ library(ggpubr)
 
 load("RData/Perennial-diversity-by-year-and-channel_v2.RData")
 load("RData/Cover-by-year-and-channel_v2.RData")
-load("RData/ANOVA-between-channels_2021.RData")
+load("RData/ANOVA-by-treatment2_2021.RData")
 
 precip <- read.table("data/PimaCounty_precip/PimaCounty_precip_2012-2021.txt",
                      sep = "\t", header = TRUE)
@@ -216,12 +216,203 @@ dev.off()
 
 # 2021 ANOVA comparisons --------------------------------------------------
 
-tiff("output_figs/SRM_2023/Soil2021.tiff", units = "in", height = 7, width = 8, res = 300)
+# Total N 
+letters <- data.frame(label = tn.letters$groups,
+                      x = 1:3,
+                      y = rep(0.64, 3))
+tn2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = TN_ppt)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Total N (mg/g soil)") +
+  ggtitle("Soil nitrogen") +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  theme(axis.text.x = element_text(color = "#000000"))
+tn2021.plot
+
+# Total C 
+letters <- data.frame(label = tc.letters$groups,
+                      x = 1:3,
+                      y = rep(6.5, 3))
+tc2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = TC_ppt)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Total C (mg/g soil)") +
+  ggtitle("Soil carbon") +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  theme(axis.text.x = element_text(color = "#000000"))
+tc2021.plot
+
+# Organic matter
+letters <- data.frame(label = om.letters$groups,
+                      x = 1:3,
+                      y = rep(2.1, 3))
+om2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = OM_perc)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Organic matter (%)") +
+  ggtitle("Soil organic matter") +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  theme(axis.text.x = element_text(color = "#000000"))
+om2021.plot
+
+# Soil bac arc richness 
+barc.rich2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = Richness.barc)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("No. of species (ASVs)") +
+  ggtitle("Soil bacterial & archaeal richness") +
+  theme(axis.text.x = element_text(color = "#000000"))
+barc.rich2021.plot
+
+# Soil bac arc diversity 
+barc.shan2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = Shannon.barc)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Shannon diversity index") + 
+  ggtitle("Soil bacterial & archaeal diversity") +
+  theme(axis.text.x = element_text(color = "#000000"))
+barc.shan2021.plot
+
+# Combine soil graphs 
+tiff("output_figs/SRM_2023/Soil2021.tiff", units = "in", height = 7, width = 9, res = 300)
 ggarrange(tn2021.plot, tc2021.plot, om2021.plot, barc.rich2021.plot,
           ncol = 2, nrow = 2)
 dev.off()
 
-tiff("output_figs/SRM_2023/Plant2021.tiff", units = "in", height = 7, width = 8, res = 300)
+
+
+# Total plant cover
+total2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = Cover)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Cover (%)") +
+  ggtitle("Total plant cover") +
+  theme(axis.text.x = element_text(color = "#000000"))
+total2021.plot
+
+# Herbaceous cover
+letters <- data.frame(label = herb.letters$groups,
+                      x = 1:3,
+                      y = rep(75, 3))
+herb2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = Herbaceous)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Cover (%)") +
+  ggtitle("Herbaceous plant cover") +
+  geom_text(data = letters,
+            mapping = aes(x = x, y = y, label = label),
+            color = "black") +
+  theme(axis.text.x = element_text(color = "#000000"))
+herb2021.plot
+
+# Perennial plant richness
+rich2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = rich)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("No. of species") +
+  ggtitle("Perennial plant richness") +
+  theme(axis.text.x = element_text(color = "#000000"))
+rich2021.plot
+
+# Perennial plant diversity 
+shan2021.plot <- ggplot(dat.2021, aes(x = Treatment2, y = shan)) +
+  geom_boxplot(aes(fill = Treatment2),
+               alpha = 0.4,
+               outlier.shape = NA) +
+  geom_jitter(aes(color = Treatment2),
+              alpha = 0.9,
+              size = 2) +
+  scale_fill_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  scale_color_manual(values = c("#33A02C", "#1F78B4", "red")) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  xlab(NULL) +
+  ylab("Shannon diversity index") +
+  ggtitle("Perennial plant diversity") +
+  theme(axis.text.x = element_text(color = "#000000"))
+shan2021.plot
+
+
+# Combine plant graphs 
+tiff("output_figs/SRM_2023/Plant2021.tiff", units = "in", height = 7, width = 9, res = 300)
 ggarrange(total2021.plot, herb2021.plot, rich2021.plot, shan2021.plot,
           ncol = 2, nrow = 2)
 dev.off()
