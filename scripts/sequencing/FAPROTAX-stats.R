7c9e3ecbc19bcfb5423f7f0b8a0b5ea643453920
Lia Ossanna <lossanna@arizona.edu>
2023-02-03 18:05:28 -0700

    Battled with faprotax for a long time but finally got reads converted to proportions (what a joke)

--- scripts/sequencing/FAPROTAX-stats.R
+++ scripts/sequencing/FAPROTAX-stats.R
@@ -4,7 +4,7 @@ library(car)
 # Load data ---------------------------------------------------------------
 
 fapro.raw <- read.table("data/cleaned/sequencing/faprotax-output.tsv",
-                        header = T)
+                        header = T, row.names = 1)
 
 meta <- read.table("data/cleaned/sequencing/sequencing_metadata.txt",
                    sep = "\t", header = TRUE, row.names = 1)
@@ -19,38 +19,21 @@ barc.asv <- read.table("data/cleaned/sequencing/bac_arc_clean_asv.txt",
 fapro0 <- fapro.raw %>% 
   select(-asv)  
 
-# Transform
+# Convert reads to proportions
+fapro0 <- (sweep(as.matrix(fapro0), 2, rowSums(barc.asv), "/")) * 100
 fapro0 <- as.data.frame(t(fapro0))
 
-# Name cols
-colnames(fapro0) <- fapro0[1, ]
-fapro0 <- fapro0[-1, ]
-
-# Add Sample col and remove row names
-fapro0$Sample <- 1:nrow(fapro0)
-fapro0 <- fapro0[ , c(93, 1:92)] # check that sample numbers match name
-row.names(fapro0) <- c()
-
-# Convert all to numeric
-str(fapro0)
-fapro0[] <- lapply(fapro0, as.numeric)
-str(fapro0)
-
-# Remove empty cols
-fapro <- fapro0 %>% 
-  select(-Sample)
-fapro <- fapro[ , colSums(fapro !=0) > 0]
-fapro <- fapro %>% 
-  mutate(Sample = c(1:62))
-
 # Add metadata
+fapro <- fapro0 %>% 
+  mutate(Sample = c(1:62)) 
+rownames(fapro) <- c()
 fapro <- left_join(meta, fapro) 
 fapro <- fapro %>% 
-  mutate(Channel = factor(fapro.all$Channel),
-         Treatment = factor(fapro.all$Treatment,
+  mutate(Channel = factor(fapro$Channel),
+         Treatment = factor(fapro$Treatment,
                             levels = c("Baffle", "One rock dam", 
                                        "Upland treatment", "No treatment")),
-         Treatment2 = factor(fapro.all$Treatment2,
+         Treatment2 = factor(fapro$Treatment2,
                              levels = c("In-channel treatment", "Upland treatment",
                                         "No treatment")))
 
@@ -58,19 +41,6 @@ fapro <- fapro %>%
 write.csv(colnames(fapro[ , -c(1:5)]),
           file = "data/cleaned/sequencing/faprotax-categories.csv",
           row.names = FALSE)
-
-# Row sums of FAPROTAX-assigned reads
-sum.fapro <- fapro0 %>% 
-  select(-Sample) 
-sum.fapro <- sum.fapro %>% 
-  mutate(sum.fapro = rowSums(sum.fapro)) %>% 
-  select(sum.fapro) %>% 
-  mutate(Sample = 1:62)
-
-# Row sums of reads from all ASVs
-sum.asv <- data.frame(Sample = 1:62,
-                      sum_asv = rowSums(barc.asv))
-  
 
 
 
