4682f7d371778d2083032e17be66fb6c5f1655a6
Lia Ossanna <lossanna@arizona.edu>
2023-02-23 16:33:50 -0700

    Continue working on percent change over time, creating clean data

--- scripts/Percent-change-over-time.R
+++ scripts/Percent-change-over-time.R
@@ -8,39 +8,73 @@ meta.raw <- read.table("data/cleaned/sequencing/sequencing_metadata.txt",
 
 # Data wrangling ----------------------------------------------------------
 
+# Format meta to connect Names with Sample no.
 meta <- meta.raw |> 
   select(Sample, Name)
-meta$Sample[1:9] <- paste0(0, meta$Sample[1:9])
-meta$Sample <- paste0("x", meta$Sample)
 
-total.diff <- total.sum |> 
+
+# Total cover -------------------------------------------------------------
+
+# Remove March samples, format to join with names from meta
+total.change <- total.sum |> 
   select(Year, Channel, Station, Cover) |> 
   filter(!str_detect(Year, "-03")) |> 
   mutate(Year = gsub("-.*", "", Year))
-total.diff$Name <- paste0(total.diff$Channel, ", ", total.diff$Station)
-total.diff <- total.diff |> 
+total.change$Name <- paste0(total.change$Channel, ", ", total.change$Station)
+total.change <- total.change |> 
   select(-Channel, -Station) |> 
   mutate(Year = as.numeric(Year))
 
-total.diff <- left_join(total.diff, meta)
-total.diff <- total.diff |> 
+# Add Sample no. from meta
+total.change <- left_join(total.change, meta)
+total.change <- total.change |> 
   select(-Name)
 
-total.diff.long <- total.diff
+total.change.long <- total.change
 
-total.diff <- total.diff.long |> 
+# Pivot wider so every column is a sample and every row is a year
+total.change.wide <- total.change.long |> 
   pivot_wider(names_from = Sample, values_from = Cover)
+total.change <- total.change.wide |> 
+  select(-Year)
 
-total.diff.12.15 <- total.diff[1:4, -c(1)]
-total.diff.12.15 <- as.matrix(total.diff.12.15)
-ts1215 <- ts(total.diff.12.15, 1, 4, frequency = 1)
+# Convert to time series object
+total.change <- as.matrix(total.change)
+totalts <- ts(total.change, 1, 6, frequency = 1)
 
-pcdf1215 <- ts1215 / stats::lag(ts1215, -1) -1
-pcdf1215 <- as.data.frame(pcdf1215)
-pcdf1215$Year <- c("2012-2013", "2013-2014", "2015-2016")
-pcdf1215 <- pcdf1215 |> 
+# Calculate percent change 
+total.pd <- totalts / stats::lag(totalts, -1) -1
+
+# Reformat as dataframe, add years, names & Treatment3
+total.pd <- as.data.frame(total.pd)
+total.pd$Year <- c("2012-2013", "2013-2014", "2014-2015", "2015-2018", "2018-2021")
+total.pd <- total.pd |> 
   pivot_longer(!Year, names_to = "Sample", values_to = "dCover")
-pcdf1215$Sample <- gsub("^.*?\\.", "", pcdf1215$Sample)
-pcdf1215 <- left_join(pcdf1215, meta)
+total.pd$Sample <- gsub("^.*?\\.", "", total.pd$Sample)
+total.pd$Sample <- as.numeric(total.pd$Sample)
+total.pd <- left_join(total.pd, meta)
+total.pd <- total.pd |> 
+  mutate(Treatment3 = case_when(
+    str_detect(total.pd$Name, "Channel 12|Channel 19") ~ "Control",
+    str_detect(total.pd$Name, "Channel 13|Channel 21") ~ "Treated"))
+
+
+
+# Percent change in total cover, 2012-2015
+total.change.12.15 <- total.change.wide[1:4, -c(1)]
+total.change.12.15 <- as.matrix(total.change.12.15)
+totalts1215 <- ts(total.change.12.15, 1, 4, frequency = 1)
+
+total.pd1215 <- totalts1215 / stats::lag(totalts1215, -1) -1
+total.pd1215 <- as.data.frame(total.pd1215)
+total.pd1215$Year <- c("2012-2013", "2013-2014", "2015-2016")
+total.pd1215 <- total.pd1215 |> 
+  pivot_longer(!Year, names_to = "Sample", values_to = "dCover")
+total.pd1215$Sample <- gsub("^.*?\\.", "", total.pd1215$Sample)
+total.pd1215 <- left_join(total.pd1215, meta)
+
+
+
+
 
 save.image("RData/Percent-change-over-time.RData")
