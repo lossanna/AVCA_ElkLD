f368d01f7a99b19ba1a34cf1bae75b06ca9d7570
Lia Ossanna <lossanna@arizona.edu>
2023-02-28 11:19:03 -0700

    Change percent difference calculation to match Munson et al. 2013, using log ratio

--- scripts/Percent-change-over-time.R
+++ scripts/Percent-change-over-time.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 
-
 # Load data ---------------------------------------------------------------
 
 total.sum <- read.csv("data/cleaned/Summarised-all_total-plant-cover.csv")
@@ -30,23 +29,27 @@ total.change <- total.change |>
 total.change <- left_join(total.change, meta)
 total.change <- total.change |> 
   select(-Name)
-
 total.change.long <- total.change
 
 # Pivot wider so every column is a sample and every row is a year
+  # separate 2012-2015 and 2015-2021
 total.change.wide <- total.change.long |> 
   pivot_wider(names_from = Sample, values_from = Cover)
-total.change <- total.change.wide |> 
-  select(-Year)
+total.change1 <- total.change.wide[1:4, -c(1)]
+total.change2 <- total.change.wide[4:6, -c(1)]
 
 # Convert to time series object
-total.change <- as.matrix(total.change)
-totalts <- ts(total.change, 1, 6, frequency = 1)
+total.change1 <- as.matrix(total.change1)
+totalts1 <- ts(total.change1, 1, 4, frequency = 1)
+total.change2 <- as.matrix(total.change2)
+totalts2 <- ts(total.change2, 1, 3, frequency = 1)
 
 # Calculate percent change 
-total.pd <- totalts / stats::lag(totalts, -1) -1
+total.pd1 <- log(totalts1) - log(stats::lag(totalts1, -1)) # 1-year interval for 2012-2015
+total.pd2 <- (log(totalts2) - log(stats::lag(totalts2 -2))) / 3 # 3-year interval for 2015-2021
 
 # Reformat as dataframe, add years, names & Treatment3
+total.pd <- rbind(total.pd1, total.pd2)
 total.pd <- as.data.frame(total.pd)
 total.pd$Year <- c("2012-2013", "2013-2014", "2014-2015", "2015-2018", "2018-2021")
 total.pd <- total.pd |> 
@@ -57,24 +60,11 @@ total.pd <- left_join(total.pd, meta)
 total.pd <- total.pd |> 
   mutate(Treatment3 = case_when(
     str_detect(total.pd$Name, "Channel 12|Channel 19") ~ "Control",
-    str_detect(total.pd$Name, "Channel 13|Channel 21") ~ "Treated"))
+    str_detect(total.pd$Name, "Channel 13|Channel 21") ~ "Treated")) |> 
+  arrange(Sample)
 
-
-# Percent change in total cover, 2012-2015
-total.change.12.15 <- total.change.wide[1:4, -c(1)]
-total.change.12.15 <- as.matrix(total.change.12.15)
-totalts1215 <- ts(total.change.12.15, 1, 4, frequency = 1)
-
-total.pd1215 <- totalts1215 / stats::lag(totalts1215, -1) -1
-total.pd1215 <- as.data.frame(total.pd1215)
-total.pd1215$Year <- c("2012-2013", "2013-2014", "2015-2016")
-total.pd1215 <- total.pd1215 |> 
-  pivot_longer(!Year, names_to = "Sample", values_to = "dCover")
-total.pd1215$Sample <- gsub("^.*?\\.", "", total.pd1215$Sample)
-total.pd1215$Sample <- as.numeric(total.pd1215$Sample)
-total.pd1215 <- left_join(total.pd1215, meta)
-
-
+write.csv(total.pd,
+          file = "data/cleaned/Percent-difference_total-cover.csv")
 
 
 
