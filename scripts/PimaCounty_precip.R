021fdd700836b6e591d60712cf1c45692e0229d1
Lia Ossanna <lossanna@arizona.edu>
2023-03-27 19:29:54 -0700

    Small edits to precip graphs

--- scripts/PimaCounty_precip.R
+++ scripts/PimaCounty_precip.R
@@ -11,9 +11,13 @@ precip <- read.table("data/PimaCounty_precip/PimaCounty_precip_2012-2021.txt",
 
 precip$year.xaxis <- as.Date(precip$year.xaxis)
 
+precip.sample <- precip |> 
+  filter(!str_detect(year.xaxis, c("2020|2016|2017|2019")))
+
 
 # Graph -------------------------------------------------------------------
 
+# Every year
 ggplot(precip, aes(x = year.xaxis, y = Precip_cum)) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
@@ -23,5 +27,15 @@ ggplot(precip, aes(x = year.xaxis, y = Precip_cum)) +
   ggtitle("Cumulative summer precipitation") +
   theme_bw() +
   scale_y_continuous(limits = c(0, 14))
-  
+
+# Sampled years only
+ggplot(precip.sample, aes(x = year.xaxis, y = Precip_cum)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3) +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  xlab(NULL) +
+  ylab("Precipitation (in)") +
+  ggtitle("Cumulative summer precipitation") +
+  theme_bw() +
+  scale_y_continuous(limits = c(0, 14))
 
