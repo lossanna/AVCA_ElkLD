230a86884e88b521c2fefb7488b61caa868b62f7
Lia Ossanna <lossanna@arizona.edu>
2023-03-29 17:42:27 -0700

    Separate total cover and herb cover PCAs

--- scripts/PCA_2021.R
+++ scripts/PCA_2021.R
@@ -12,7 +12,8 @@ dat.2021 <- read_csv("data/cleaned/Data-2021_clean.csv")
 # Data wrangling ----------------------------------------------------------
 
 # Narrow down variables
-dat.pca <- dat.2021 |> 
+# All, total cover
+dat.total.all <- dat.2021 |> 
   select(Cover, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
   rename(TN = TN_log,
          TC = TC_log,
@@ -20,7 +21,36 @@ dat.pca <- dat.2021 |>
          Richness = rich,
          Shannon = shan)
 
-dat.pca.trt <- dat.2021 |> 
+# By Treated/Control, total cover
+dat.total.trt <- dat.2021 |> 
+  filter(Treatment3 == "Treated") |> 
+  select(Cover, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
+  rename(TN = TN_log,
+         TC = TC_log,
+         OM = OM_perc,
+         Richness = rich,
+         Shannon = shan)
+
+dat.total.ctrl <- dat.2021 |> 
+  filter(Treatment3 == "Control") |> 
+  select(Herbaceous, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
+  rename(TN = TN_log,
+         TC = TC_log,
+         OM = OM_perc,
+         Richness = rich,
+         Shannon = shan)
+
+# All, herbaceous cover
+dat.herb.all <- dat.2021 |> 
+  select(Herbaceous, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
+  rename(TN = TN_log,
+         TC = TC_log,
+         OM = OM_perc,
+         Richness = rich,
+         Shannon = shan)
+
+# By Treated/Control, herbaceous cover
+dat.herb.trt <- dat.2021 |> 
   filter(Treatment3 == "Treated") |> 
   select(Herbaceous, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
   rename(TN = TN_log,
@@ -29,7 +59,7 @@ dat.pca.trt <- dat.2021 |>
          Richness = rich,
          Shannon = shan)
 
-dat.pca.ctrl <- dat.2021 |> 
+dat.herb.ctrl <- dat.2021 |> 
   filter(Treatment3 == "Control") |> 
   select(Herbaceous, rich, shan, TN_log, TC_log, OM_perc, Richness.barc, Shannon.barc) |> 
   rename(TN = TN_log,
@@ -39,33 +69,33 @@ dat.pca.ctrl <- dat.2021 |>
          Shannon = shan)
 
 
-# PCA by Treatment3 -------------------------------------------------------
 
+# PCA by Treatment3, total cover ------------------------------------------
 
 # All
-PCA(dat.pca, scale.unit = TRUE, graph = TRUE)
-pca.all <- PCA(dat.pca, scale.unit = TRUE, graph = FALSE)
+PCA(dat.total.all, scale.unit = TRUE, graph = TRUE)
+pca.total.all <- PCA(dat.total.all, scale.unit = TRUE, graph = FALSE)
 
-get_eigenvalue(pca.all)
+get_eigenvalue(pca.total.all)
 
-var.all <- get_pca_var(pca.all)
-head(var.all$coord) # coordinates
-head(var.all$cos2)
-head(var.all$contrib)
+var.total.all <- get_pca_var(pca.total.all)
+head(var.total.all$coord) # coordinates
+head(var.total.all$cos2)
+head(var.total.all$contrib)
 
-fviz_pca_var(pca.all,
+fviz_pca_var(pca.total.all,
              repel = TRUE)
 
-ind.all <- get_pca_ind(pca.all)
-fviz_pca_ind(pca.all)
+ind.total.all <- get_pca_ind(pca.total.all)
+fviz_pca_ind(pca.total.all)
 
-fviz_pca_ind(pca.all,
+fviz_pca_ind(pca.total.all,
              geom.ind = "point",
              col.ind = dat.2021$Treatment3,
              palette = c("red", "#1F78B4"),
              addEllipses = TRUE)
 
-fviz_pca_ind(pca.all,
+fviz_pca_ind(pca.total.all,
              geom.ind = "point",
              col.ind = dat.2021$Channel,
              palette = c("red", "#1F78B4", "red", "#1F78B4"),
@@ -74,32 +104,98 @@ fviz_pca_ind(pca.all,
 
 # Separate Control & Treatment
 # Control
-pca.ctrl3 <- PCA(dat.pca.ctrl, scale.unit = TRUE, graph = FALSE)
-get_eigenvalue(pca.ctrl3)
+pca.total.ctrl3 <- PCA(dat.total.ctrl, scale.unit = TRUE, graph = FALSE)
+get_eigenvalue(pca.total.ctrl3)
 
-fviz_pca_var(pca.ctrl3,
+fviz_pca_var(pca.total.ctrl3,
              repel = TRUE) +
   labs(title = "PCA for Control")
 
-var.ctrl3 <- get_pca_var(pca.ctrl3) # variable contributions
-var.ctrl3$contrib
-fviz_contrib(pca.ctrl3, choice = "var", axes = 1, top = 10) +
+var.total.ctrl3 <- get_pca_var(pca.total.ctrl3) # variable contributions
+var.total.ctrl3$contrib
+fviz_contrib(pca.total.ctrl3, choice = "var", axes = 1, top = 10) +
   labs(title = "Var contribution PC1 Control") # contrib PC1
-fviz_contrib(pca.ctrl3, choice = "var", axes = 2, top = 10) +
+fviz_contrib(pca.total.ctrl3, choice = "var", axes = 2, top = 10) +
   labs(title = "Var contribution PC2 Control") # contrib PC2
 
 
 # Treated
-pca.trt3 <- PCA(dat.pca.trt, scale.unit = TRUE, graph = FALSE)
-get_eigenvalue(pca.trt3)
+pca.total.trt3 <- PCA(dat.total.trt, scale.unit = TRUE, graph = FALSE)
+get_eigenvalue(pca.total.trt3)
 
-fviz_pca_var(pca.trt3,
+fviz_pca_var(pca.total.trt3,
              repel = TRUE) +
   labs(title = "PCA for Treated")
 
-var.trt3 <- get_pca_var(pca.trt3) # variable contributions
-var.trt3$contrib
-fviz_contrib(pca.trt3, choice = "var", axes = 1, top = 10) # contrib PC1
-fviz_contrib(pca.trt3, choice = "var", axes = 2, top = 10) # contrib PC2
+var.total.trt3 <- get_pca_var(pca.total.trt3) # variable contributions
+var.total.trt3$contrib
+fviz_contrib(pca.total.trt3, choice = "var", axes = 1, top = 10) # contrib PC1
+fviz_contrib(pca.total.trt3, choice = "var", axes = 2, top = 10) # contrib PC2
+
+
+# PCA by Treatment3, herb cover -------------------------------------------
+
+# All
+PCA(dat.herb.all, scale.unit = TRUE, graph = TRUE)
+pca.herb.all <- PCA(dat.herb.all, scale.unit = TRUE, graph = FALSE)
+
+get_eigenvalue(pca.herb.all)
+
+var.herb.all <- get_pca_var(pca.herb.all)
+head(var.herb.all$coord) # coordinates
+head(var.herb.all$cos2)
+head(var.herb.all$contrib)
+
+fviz_pca_var(pca.herb.all,
+             repel = TRUE)
+
+ind.herb.all <- get_pca_ind(pca.herb.all)
+fviz_pca_ind(pca.herb.all)
+
+fviz_pca_ind(pca.herb.all,
+             geom.ind = "point",
+             col.ind = dat.2021$Treatment3,
+             palette = c("red", "#1F78B4"),
+             addEllipses = TRUE)
+
+fviz_pca_ind(pca.herb.all,
+             geom.ind = "point",
+             col.ind = dat.2021$Channel,
+             palette = c("red", "#1F78B4", "red", "#1F78B4"),
+             addEllipses = TRUE)
+
+
+# Separate Control & Treatment
+# Control
+pca.herb.ctrl3 <- PCA(dat.herb.ctrl, scale.unit = TRUE, graph = FALSE)
+get_eigenvalue(pca.herb.ctrl3)
+
+fviz_pca_var(pca.herb.ctrl3,
+             repel = TRUE) +
+  labs(title = "PCA for Control")
+
+var.herb.ctrl3 <- get_pca_var(pca.herb.ctrl3) # variable contributions
+var.herb.ctrl3$contrib
+fviz_contrib(pca.herb.ctrl3, choice = "var", axes = 1, top = 10) +
+  labs(title = "Var contribution PC1 Control") # contrib PC1
+fviz_contrib(pca.herb.ctrl3, choice = "var", axes = 2, top = 10) +
+  labs(title = "Var contribution PC2 Control") # contrib PC2
+
+
+# Treated
+pca.herb.trt3 <- PCA(dat.herb.trt, scale.unit = TRUE, graph = FALSE)
+get_eigenvalue(pca.herb.trt3)
+
+fviz_pca_var(pca.herb.trt3,
+             repel = TRUE) +
+  labs(title = "PCA for Treated")
+
+var.herb.trt3 <- get_pca_var(pca.herb.trt3) # variable contributions
+var.herb.trt3$contrib
+fviz_contrib(pca.herb.trt3, choice = "var", axes = 1, top = 10) # contrib PC1
+fviz_contrib(pca.herb.trt3, choice = "var", axes = 2, top = 10) # contrib PC2
+
+
 
 save.image("RData/PCA_2021.RData")
+
