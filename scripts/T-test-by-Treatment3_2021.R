72bccb230df072d174228ab6e888c97a1c78a120
Lia Ossanna <lossanna@arizona.edu>
2023-04-13 09:56:19 -0700

    Make nicer graphs for 2021 soil & plant values

--- scripts/T-test-by-Treatment3_2021.R
+++ scripts/T-test-by-Treatment3_2021.R
@@ -1,4 +1,5 @@
 # Purpose: Compare 2021 soil and plant data by Treatment3 (Treated/Control).
+#   Used t-test because determined all were normally distributed in Data-screening_2021.R
 
 library(tidyverse)
 library(car)
@@ -11,100 +12,224 @@ dat.2021 <- read.csv("data/cleaned/Data-2021_clean.csv")
 
 # Total N -----------------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$TN_log,
        filter(dat.2021, Treatment3 == "Treated")$TN_log) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = TN_ppt)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+tn.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = TN_ppt, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Total soil nitrogen",
+       x = NULL,
+       y = "Total N (mg/g)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none")
+tn.plot.21
 
 
 # Total C -----------------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$TC_log,
        filter(dat.2021, Treatment3 == "Treated")$TC_log) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = TC_ppt)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+tc.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = TC_ppt, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Total soil carbon",
+       x = NULL,
+       y = "Total C (mg/g)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none")
+tc.plot.21
 
 
 
 # C:N ratio ---------------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$CN_ratio,
        filter(dat.2021, Treatment3 == "Treated")$CN_ratio) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = CN_ratio)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+cn.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = CN_ratio, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Carbon:Nitrogen ratio",
+       x = NULL,
+       y = "C:N") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none")
+cn.plot.21
 
 
 # Organic matter ----------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$OM_perc,
        filter(dat.2021, Treatment3 == "Treated")$OM_perc) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = OM_perc)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+om.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = OM_perc, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Soil organic matter",
+       x = NULL,
+       y = "Organic matter (%)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+om.plot.21
 
 
 # Soil bac arc richness ---------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$Richness.barc,
        filter(dat.2021, Treatment3 == "Treated")$Richness.barc) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = Richness.barc)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+barcrich.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = Richness.barc, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Bacteria & archaea richness",
+       x = NULL,
+       y = "No. of ASVs") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none")
+barcrich.plot.21
 
 
 # Soil bac arc diversity --------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$Shannon.barc,
        filter(dat.2021, Treatment3 == "Treated")$Shannon.barc) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = Shannon.barc)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+barcshan.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = Shannon.barc, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Bacteria & archaea diversity",
+       x = NULL,
+       y = "Shannon diversity index") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none")
+barcshan.plot.21
 
 
 
 # Total plant cover -------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$Cover,
        filter(dat.2021, Treatment3 == "Treated")$Cover) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = Cover)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+total.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = Cover, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Total plant cover",
+       x = NULL,
+       y = "Cover (%)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+total.plot.21
 
 
 # Herbaceous cover --------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$Herbaceous,
        filter(dat.2021, Treatment3 == "Treated")$Herbaceous) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = Herbaceous)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+herb.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = herb, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Herbaceous cover",
+       x = NULL,
+       y = "Cover (%)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+herb.plot.21
 
 
 # Notree cover ------------------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$notree,
        filter(dat.2021, Treatment3 == "Treated")$notree) # NS
+
+# Plot
+notree.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = notree, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Grass, forb & shrub cover",
+       x = NULL,
+       y = "Cover (%)") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+notree.plot.21
 
 
 # Perennial plant richness ------------------------------------------------
 
+# T-test
 t.test(filter(dat.2021, Treatment3 == "Control")$rich,
        filter(dat.2021, Treatment3 == "Treated")$rich) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = rich)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+rich.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = rich, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Perennial plant richness",
+       x = NULL,
+       y = "No. of species") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+rich.plot.21
 
 
 
@@ -113,6 +238,20 @@ ggplot(dat.2021, aes(x = Treatment3, y = rich)) +
 t.test(filter(dat.2021, Treatment3 == "Control")$shan,
        filter(dat.2021, Treatment3 == "Treated")$shan) # NS
 
-ggplot(dat.2021, aes(x = Treatment3, y = shan)) +
-  geom_boxplot() +
-  geom_jitter()
+# Plot
+shan.plot.21 <- dat.2021 |> 
+  ggplot(aes(x = Treatment3, y = shan, fill = Treatment3, color = Treatment3)) +
+  geom_boxplot(alpha = 0.3,
+               outlier.shape = NA) +
+  geom_jitter(size = 2) +
+  scale_color_manual(values = c("red", "#1F78B4")) +
+  scale_fill_manual(values = c("red", "#1F78B4")) +
+  labs(title = "Perennial plant diversity",
+       x = NULL,
+       y = "Shannon diversity index") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") 
+shan.plot.21
+
+
+save.image("RData/T-test-by-Treatment3_2021.RData")
