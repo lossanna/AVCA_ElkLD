c8bcec038db034cb8bcff847a867f8fa145eeef2
Lia Ossanna <lossanna@arizona.edu>
2023-05-23 14:44:45 -0700

    Add metadata, both as separate csv and also to elevation df

--- scripts/Cross-section-elevation.R
+++ scripts/Cross-section-elevation.R
@@ -2,6 +2,7 @@ library(tidyverse)
 
 # Load data ---------------------------------------------------------------
 
+# Raw elevation data
 setwd("data/cross-section_raw/") # setwd() to get list.files() to work
 #   including the relative path directly in list.files() does not work; it needs absolute path
 #   or defaults to current working directory
@@ -11,6 +12,8 @@ elev.raw <- list.files(pattern = "CH*") %>%
 setwd("C:/Users/liaos/OneDrive - University of Arizona/grad school/Gornish lab/02_AVCA Elkhorn-Las Delicias/AVCA_ElkLD")
 #   setwd() back to project directory
 
+# Station metadata
+station.metadata <- read_csv("data/station-metadata.csv")
 
 # Fix col name typo -------------------------------------------------------
 
@@ -28,13 +31,34 @@ elev <- bind_rows(elev, elevatin2011)
 
 
 
-# Rename columns, Station & Channel ---------------------------------------
+# Rename columns, standardize names ---------------------------------------
 
 # Rename columns
 colnames(elev) <- c("Station.elev", "Channel.elev", "Distance", "elev2011", "elev2019")
 
+# Standardize names
 channel.station.names <- elev |> 
   select(Channel.elev, Station.elev) |> 
-  unique()
+  unique() 
 write_csv(channel.station.names,
           file = "data/cross-section_raw/Names_raw.csv")
+
+# **Translate/standardize names manually in a separate file**
+channel.station.names.translate <- read_csv("data/cross-section_raw/Names_translated.csv")
+
+# Join dfs to assign names
+elev <- left_join(elev, channel.station.names.translate)
+
+
+# Add metadata ------------------------------------------------------------
+
+elev <- elev |> 
+  select(-Channel.elev, -Station.elev) |> 
+  left_join(station.metadata) |> 
+  arrange(Sample) |> 
+  select(Sample, Name, Channel, Station, Treatment1, Treatment2, Treatment3, Distance, elev2011, elev2019)
+
+
+# Find minimum elevation for each station from 2011 -----------------------
+
+
