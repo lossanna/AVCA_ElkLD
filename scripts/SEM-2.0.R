b5f028d696f7784a971a373d5dacd9e71b594ec1
Lia Ossanna <lossanna@arizona.edu>
2023-06-27 18:23:27 -0700

    Begin SEM2.0 analysis; scale & center variables, test initial model and try to solve negative variance issue

---
+++ scripts/SEM-2.0.R
@@ -0,0 +1,115 @@
+# Purpose: SEM analysis
+# Created: 2023-06-27
+# Updated: 2023-06-27
+
+library(lavaan)
+library(tidyverse)
+
+# Load data ---------------------------------------------------------------
+
+dat.2021 <- read.csv("data/cleaned/Data-2021_clean.csv") 
+
+
+# Data wrangling ----------------------------------------------------------
+
+# Add Control/Treated as binary variable and select only variables needed for SEM
+sem.dat.unscaled <- dat.2021 |> 
+  mutate(rocks = case_when(
+    Treatment3 == "Control" ~ 0,
+    Treatment3 == "Treated" ~ 1)) |> 
+  select(Sample, rocks, notree, perveg.richness, perveg.shannon,
+         TN_log, OM_log, OM_perc, barc.richness, fungi.richness,
+         chemoheterotrophy_log, n.cycler_log, saprotroph) 
+
+# Center and scale variables
+sem.dat <- sem.dat.unscaled |> 
+  mutate(rocks = as.factor(rocks),
+         Sample = as.factor(Sample)) |> 
+  mutate_if(is.numeric, scale)
+
+
+# SEM, plants as latent variable ------------------------------------------
+
+# Initial attempt
+mod1.0 <- '
+  # latent variables
+  soil_microbe =~ barc.richness + fungi.richness + chemoheterotrophy_log + n.cycler_log + saprotroph
+  soil_chem =~ TN_log + OM_log
+  plants =~ notree + perveg.richness + perveg.shannon
+  
+  # structure
+  plants ~ rocks + soil_chem 
+  soil_chem ~ rocks + soil_microbe
+  soil_microbe ~ rocks
+'
+fit <- sem(mod1.0, data = sem.dat) # lavaan WARNING: some estimated ov variances are negative
+summary(fit) 
+#   possibly this is because some variables are collinear
+
+
+# Remove TN 
+#   solves problem, but model loses important information
+mod1.1 <- '
+  # latent variables
+  soil_microbe =~ barc.richness + fungi.richness + chemoheterotrophy_log + n.cycler_log + saprotroph
+  soil_chem =~ OM_log
+  plants =~ notree + perveg.richness + perveg.shannon
+  
+  # structure
+  plants ~ rocks + soil_chem 
+  soil_chem ~ rocks + soil_microbe
+  soil_microbe ~ rocks
+'
+fit <- sem(mod1.1, data = sem.dat)
+summary(fit) # removing TN solves negative variance warning
+
+
+# Change OM_log to OM_perc
+#   does not solve problem
+mod1.2 <- '
+  # latent variables
+  soil_microbe =~ barc.richness + fungi.richness + chemoheterotrophy_log + n.cycler_log + saprotroph
+  soil_chem =~ OM_perc + TN_log
+  plants =~ notree + perveg.richness + perveg.shannon
+  
+  # structure
+  plants ~ rocks + soil_chem 
+  soil_chem ~ rocks + soil_microbe
+  soil_microbe ~ rocks
+'
+fit <- sem(mod1.2, data = sem.dat) # lavaan WARNING: some estimated ov variances are negative
+summary(fit) # using OM_perc doesn't solve variance issue
+
+
+# Remove OM_log
+#   solves problem
+mod1.3 <- '
+  # latent variables
+  soil_microbe =~ barc.richness + fungi.richness + chemoheterotrophy_log + n.cycler_log + saprotroph
+  soil_chem =~ TN_log
+  plants =~ notree + perveg.richness + perveg.shannon
+  
+  # structure
+  plants ~ rocks + soil_chem 
+  soil_chem ~ rocks + soil_microbe
+  soil_microbe ~ rocks
+'
+fit <- sem(mod1.3, data = sem.dat) # lavaan WARNING: some estimated ov variances are negative
+summary(fit) # removing OM_log solves negative variance issue
+
+
+# Break include OM & TN as separate indicator variables, no latent
+#   does not solve problem
+mod1.4 <- '
+  # latent variables
+  soil_microbe =~ barc.richness + fungi.richness + chemoheterotrophy_log + n.cycler_log + saprotroph
+  plants =~ notree + perveg.richness + perveg.shannon
+  
+  # structure
+  plants ~ rocks + TN_log + OM_log
+  TN_log ~ soil_microbe + rocks
+  OM_log ~ soil_microbe + rocks
+  soil_microbe ~ rocks
+'
+fit <- sem(mod1.4, data = sem.dat) # lavaan WARNING: some estimated ov variances are negative
+summary(fit) # removing soil_chem as latent variable does not solve variance issue
