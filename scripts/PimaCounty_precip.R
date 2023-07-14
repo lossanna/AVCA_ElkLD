da7d7de2c9d5895fbcd7dcff0c24f22548519616
Lia Ossanna <lossanna@arizona.edu>
2023-07-14 12:10:42 -0600

    Write out precip line graph

--- scripts/PimaCounty_precip.R
+++ scripts/PimaCounty_precip.R
@@ -3,7 +3,7 @@
 # Created: 2023-02-02
 # Last updated: 2023-03-27
 
-library(ggplot2)
+library(tidyverse)
 library(plotrix)
 
 # Load data ---------------------------------------------------------------
@@ -23,15 +23,17 @@ precip.sample <- precip |>
 # Graph -------------------------------------------------------------------
 
 # Every year
+tiff("figures/2023-07_draft-figures/Precipitation-line-graph.tiff", width = 6, height = 4, units = "in", res = 150)
 ggplot(precip, aes(x = year.xaxis, y = Precip_cum)) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
-  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
   xlab(NULL) +
-  ylab("Precipitation (in)") +
-  ggtitle("Cumulative summer precipitation") +
+  ylab("Precipitation (in.)") +
+  ggtitle("Cumulative summer precipitation (June-October)") +
   theme_bw() +
-  scale_y_continuous(limits = c(0, 14))
+  scale_y_continuous(limits = c(0, 14)) +
+  theme(axis.text.x = element_text(color = "black"))
+dev.off()
 
 # Sampled years only
 ggplot(precip.sample, aes(x = year.xaxis, y = Precip_cum)) +
