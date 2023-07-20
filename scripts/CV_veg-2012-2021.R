2643a34c15f19e14314271a06a473a3a3790ea7c
Lia Ossanna <lossanna@arizona.edu>
2023-07-20 13:02:35 -0600

    Explore CV distribution and add log-transformation when needed. Write CV of notree, richness, and Shannon to figure

--- scripts/CV_veg-2012-2021.R
+++ scripts/CV_veg-2012-2021.R
@@ -1,13 +1,18 @@
 # Purpose: Calculate CV of each sample across all years, and compare CVs of Treated vs. Control.
 #   CV is a measure of stability and resistance to change.
-# Did not find any significant differences in CVs for total cover, herb cover, richness, or Shannon.
-# Created: 2022-02-15
-# Last updated: 2023-05-26
 
+# Tested normality distribution and log-transformed herb cover, richness & Shannon.
+#   Compared with t-test, using log-transformation if needed.
+#   
+# Did not find any significant differences in CVs for total, herb, notree, richness, or Shannon.
+
+# Created: 2022-02-15
+# Last updated: 2023-07-20
 
 library(tidyverse)
 library(car)
 library(scales)
+library(ggpubr)
 
 # Load data ---------------------------------------------------------------
 
@@ -19,23 +24,23 @@ per.div <- read.csv("data/cleaned/Summarised-all_perennial-diversity.csv")
 
 # Total cover -------------------------------------------------------------
 
+# Find CV for each sample over time
 total.sample <- total.all |> 
   group_by(Sample, Channel, Station, Treatment3) |> 
   summarise(CV = sd(Cover) / mean(Cover),
             .groups = "keep")
 
-write_csv(total.sample,
-          "data/cleaned/CV-2012-2021_total-cover.csv")
-
-summary(filter(total.sample, Treatment3 == "Treated")$CV)
-summary(filter(total.sample, Treatment3 == "Control")$CV)
-
+# Explore distribution
 qqPlot(filter(total.sample, Treatment3 == "Treated")$CV) # normal
 qqPlot(filter(total.sample, Treatment3 == "Control")$CV) # normal
 
+# Write to csv
+write_csv(total.sample,
+          "data/cleaned/CV-2012-2021_total-cover.csv")
+
 # Compare
 t.test(filter(total.sample, Treatment3 == "Treated")$CV,
-       filter(total.sample, Treatment3 == "Control")$CV) # NS
+       filter(total.sample, Treatment3 == "Control")$CV) # NS, p = 0.259
 
 # Plot
 total.plot.cv <- total.sample |> 
@@ -55,26 +60,33 @@ total.plot.cv
 
 # Herb cover --------------------------------------------------------------
 
+# Find CV for each sample over time
 herb.sample <- herb.all |> 
   group_by(Sample, Channel, Station, Treatment3) |> 
   summarise(CV = sd(Cover) / mean(Cover),
             .groups = "keep")
 
+# Explore distribution
+qqPlot(filter(herb.sample, Treatment3 == "Treated")$CV) # normal
+qqPlot(filter(herb.sample, Treatment3 == "Control")$CV) # not quite normal?
+
+# Log transformation sort of helps Control
+qqPlot(log(filter(herb.sample, Treatment3 == "Control")$CV)) # more normal?
+qqPlot(log(filter(herb.sample, Treatment3 == "Treated")$CV)) # normal
+
+herb.sample <- herb.sample |> 
+  mutate(CV_log = log(CV))
+
+# Write to csv
 write_csv(herb.sample,
           "data/cleaned/CV-2012-2021_herb-cover.csv")
 
-summary(filter(herb.sample, Treatment3 == "Treated")$CV)
-summary(filter(herb.sample, Treatment3 == "Control")$CV)
-
-qqPlot(filter(herb.sample, Treatment3 == "Treated")$CV) # normal
-qqPlot(filter(herb.sample, Treatment3 == "Control")$CV) # kind of normal?
-
 # Compare
 wilcox.test(filter(herb.sample, Treatment3 == "Treated")$CV,
-            filter(herb.sample, Treatment3 == "Control")$CV) # NS
+            filter(herb.sample, Treatment3 == "Control")$CV) # NS, p = 0.148
 
-t.test(filter(herb.sample, Treatment3 == "Treated")$CV,
-       filter(herb.sample, Treatment3 == "Control")$CV) # NS
+t.test(filter(herb.sample, Treatment3 == "Treated")$CV_log,
+       filter(herb.sample, Treatment3 == "Control")$CV_log) # NS, p = 0.122
 
 # Plot
 herb.plot.cv <- herb.sample |> 
@@ -95,113 +107,164 @@ herb.plot.cv
 
 # Notree cover ------------------------------------------------------------
 
+# Find CV for each sample over time
 notree.sample <- notree.all |> 
   group_by(Sample, Channel, Station, Treatment3) |> 
   summarise(CV = sd(Cover) / mean(Cover),
             .groups = "keep")
 
-write_csv(notree.sample,
-          "data/cleaned/CV-2012-2021_notree-cover.csv")
-
-summary(filter(notree.sample, Treatment3 == "Treated")$CV)
-summary(filter(notree.sample, Treatment3 == "Control")$CV)
-
+# Explore distribution
 qqPlot(filter(notree.sample, Treatment3 == "Treated")$CV) # normal
 qqPlot(filter(notree.sample, Treatment3 == "Control")$CV) # normal
 
+# Write to csv
+write_csv(notree.sample,
+          "data/cleaned/CV-2012-2021_notree-cover.csv")
+
 # Compare
 t.test(filter(notree.sample, Treatment3 == "Treated")$CV,
-       filter(notree.sample, Treatment3 == "Control")$CV) # NS
+       filter(notree.sample, Treatment3 == "Control")$CV) # NS, p = 0.882
 
 # Plot
 notree.plot.cv <- notree.sample |> 
-  ggplot(aes(x = Treatment3, y = CV, fill = Treatment3, color = Treatment3)) +
+  ggplot(aes(x = Treatment3, y = CV)) +
   geom_boxplot(alpha = 0.3,
-               outlier.shape = NA) +
-  geom_jitter(size = 2) +
+               outlier.shape = NA,
+               aes(fill = Treatment3)) +
+  geom_jitter(size = 2,
+              alpha = 0.8,
+              aes(color = Treatment3)) +
   scale_color_manual(values = c("red", "#1F78B4")) +
   scale_fill_manual(values = c("red", "#1F78B4")) +
-  labs(title = "Grass, forb & shrub CV",
-       x = NULL) +
-  theme_bw(base_size = 14) +
+  labs(title = "Grass, forb & shrub \ncover",
+       x = NULL,
+       y = "Coefficient of variation") +
+  theme_bw() +
   theme(legend.position = "none") +
-  scale_y_continuous(labels = percent)
+  scale_y_continuous(labels = percent) +
+  theme(axis.text.x = element_text(color = "black")) +
+  geom_text(aes(x = 1.5, y = 0.8, label = "ANOVA, p = 0.882"),
+            color = "gray30",
+            size = 2.5) +
+  theme(plot.margin = margin(0.1, 0.2, 0.1, 0.1, "in"))
 notree.plot.cv
 
 
 
 # Richness ----------------------------------------------------------------
 
+# Find CV for each sample over time
 rich.sample <- per.div |> 
   group_by(Sample, Channel, Station, Treatment3) |> 
   summarise(CV = sd(rich) / mean(rich),
             .groups = "keep")
 
+# Explore distribution
+qqPlot(filter(rich.sample, Treatment3 == "Treated")$CV) # almost normal?
+qqPlot(filter(rich.sample, Treatment3 == "Control")$CV) # normal
+
+# Log transformation helps Treated 
+qqPlot(log(filter(rich.sample, Treatment3 == "Treated")$CV)) # normal now
+qqPlot(log(filter(rich.sample, Treatment3 == "Control")$CV)) # normal
+
+rich.sample <- rich.sample |> 
+  mutate(CV_log = log(CV))
+
+# Write to csv
 write_csv(rich.sample,
           "data/cleaned/CV-2012-2021_richness.csv")
 
-summary(filter(rich.sample, Treatment3 == "Treated")$CV)
-summary(filter(rich.sample, Treatment3 == "Control")$CV)
-
-qqPlot(filter(rich.sample, Treatment3 == "Treated")$CV) # normal
-qqPlot(filter(rich.sample, Treatment3 == "Control")$CV) # normal
-
 # Compare
-t.test(filter(rich.sample, Treatment3 == "Treated")$CV,
-       filter(rich.sample, Treatment3 == "Control")$CV) # NS
+t.test(filter(rich.sample, Treatment3 == "Treated")$CV_log,
+       filter(rich.sample, Treatment3 == "Control")$CV_log) # NS, p = 0.093
 
 # Plot
 rich.plot.cv <- rich.sample |> 
-  ggplot(aes(x = Treatment3, y = CV, fill = Treatment3, color = Treatment3)) +
+  ggplot(aes(x = Treatment3, y = CV)) +
   geom_boxplot(alpha = 0.3,
-               outlier.shape = NA) +
-  geom_jitter(size = 2) +
+               outlier.shape = NA,
+               aes(fill = Treatment3)) +
+  geom_jitter(size = 2,
+              alpha = 0.8,
+              aes(color = Treatment3)) +
   scale_color_manual(values = c("red", "#1F78B4")) +
   scale_fill_manual(values = c("red", "#1F78B4")) +
-  labs(title = "Richness CV",
-       x = NULL) +
-  theme_bw(base_size = 14) +
+  labs(title = "Perennial plant species \nrichness",
+       x = NULL,
+       y = "Coefficient of variation") +
+  theme_bw() +
   theme(legend.position = "none") +
-  scale_y_continuous(labels = percent)
+  scale_y_continuous(labels = percent) +
+  theme(axis.text.x = element_text(color = "black")) +
+  geom_text(aes(x = 1.03, y = 0.55, label = "ANOVA, p = 0.093"),
+            color = "gray30",
+            size = 2.5) +
+  theme(plot.margin = margin(0.1, 0.2, 0.1, 0.1, "in"))
 rich.plot.cv
 
 
 
 # Shannon -----------------------------------------------------------------
 
+# Find CV for each sample over time
 shan.sample <- per.div |> 
   group_by(Sample, Channel, Station, Treatment3) |> 
   summarise(CV = sd(shan) / mean(shan),
             .groups = "keep")
 
-write_csv(shan.sample,
-          "data/cleaned/CV-2012-2021_shannon.csv")
-
-summary(filter(shan.sample, Treatment3 == "Treated")$CV)
-summary(filter(shan.sample, Treatment3 == "Control")$CV)
-
+# Explore distribution
 qqPlot(filter(shan.sample, Treatment3 == "Treated")$CV) # normal
 qqPlot(filter(shan.sample, Treatment3 == "Control")$CV) # almost normal
 
+# Log transformation helps Control
+qqPlot(log(filter(shan.sample, Treatment3 == "Control")$CV)) # normal now
+qqPlot(log(filter(shan.sample, Treatment3 == "Treated")$CV)) # normal
+
+shan.sample <- shan.sample |> 
+  mutate(CV_log = log(CV))
+
+# Write to csv
+write_csv(shan.sample,
+          "data/cleaned/CV-2012-2021_shannon.csv")
+
 # Compare
-t.test(filter(shan.sample, Treatment3 == "Treated")$CV,
-       filter(shan.sample, Treatment3 == "Control")$CV) # NS
+t.test(filter(shan.sample, Treatment3 == "Treated")$CV_log,
+       filter(shan.sample, Treatment3 == "Control")$CV_log) # NS, p = 0.075
 
 # Plot
 shan.plot.cv <- shan.sample |> 
-  ggplot(aes(x = Treatment3, y = CV, fill = Treatment3, color = Treatment3)) +
+  ggplot(aes(x = Treatment3, y = CV)) +
   geom_boxplot(alpha = 0.3,
-               outlier.shape = NA) +
-  geom_jitter(size = 2) +
+               outlier.shape = NA,
+               aes(fill = Treatment3)) +
+  geom_jitter(size = 2,
+              alpha = 0.8,
+              aes(color = Treatment3)) +
   scale_color_manual(values = c("red", "#1F78B4")) +
   scale_fill_manual(values = c("red", "#1F78B4")) +
-  labs(title = "Shannon diversity CV",
-       x = NULL) +
-  theme_bw(base_size = 14) +
+  labs(title = "Shannon diversity index",
+       x = NULL,
+       y = "Coefficient of variation") +
+  theme_bw() +
   theme(legend.position = "none") +
-  scale_y_continuous(labels = percent)
+  scale_y_continuous(labels = percent) +
+  theme(axis.text.x = element_text(color = "black")) +
+  geom_text(aes(x = 1.03, y = 0.75, label = "ANOVA, p = 0.075"),
+            color = "gray30",
+            size = 2.5) +
+  theme(plot.margin = margin(0.1, 0.2, 0.1, 0.1, "in"))
 shan.plot.cv
 
 
-save.image("RData/CV_veg-2023-2021.RData")
+
+# Combine notree, richness & Shannon --------------------------------------
+
+tiff("figures/2023-07_draft-figures/CV.tiff", units = "in", height = 4.7, width = 8, res = 150)
+ggarrange(notree.plot.cv, rich.plot.cv, shan.plot.cv,
+          ncol = 3, nrow = 1,
+          labels = c("(A)", "(B)", "(C)")) 
+
+dev.off()
+
+save.image("RData/CV_veg-2012-2021.RData")
 
