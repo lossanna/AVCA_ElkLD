06e5aae7df70cdefa46fd883791836e04e450831
Lia Ossanna <lossanna@arizona.edu>
2023-07-20 11:52:31 -0600

    Write out plot of correlation between precipitation and vegetation

--- scripts/Correlation.R
+++ scripts/Correlation.R
@@ -1,9 +1,10 @@
 # Purpose: Explore correlation between notree averages and precip, and 
 #   elevation difference and percent difference (but not sure this will really work).
 # Created: 2023-05-24
-# Last updated: 2023-07-14
+# Last updated: 2023-07-20
 
 library(tidyverse)
+library(ggpubr)
 
 # Load data ---------------------------------------------------------------
 
@@ -42,21 +43,39 @@ dat.elev <- left_join(elev, firstlast)
 # Visualization -----------------------------------------------------------
 
 # Precip & veg cover
-notree.ctrl |> 
+notree.ctrl.plot <- notree.ctrl |> 
   ggplot(aes(x = Precip_cum, y = mean)) +
   geom_point() +
   geom_smooth(method = "lm") +
   labs(x = "Cummulative summer precipitation (in.)",
        y = "Grass, forb & shrub cover (%)",
-       title = "Control")
+       title = "Control") +
+  theme_bw() +
+  stat_regline_equation(label.x = 7.5, label.y = 46.5) +
+  stat_cor(label.x = 7.5, label.y = 43.5) 
+notree.ctrl.plot
+  
 
-notree.trt |> 
+notree.trt.plot <- notree.trt |> 
   ggplot(aes(x = Precip_cum, y = mean)) +
   geom_point() +
   geom_smooth(method = "lm") +
   labs(x = "Cummulative summer precipitation (in.)",
        y = "Grass, forb & shrub cover (%)",
-       title = "Treated")
+       title = "Treated") +
+  theme_bw() +
+  stat_regline_equation(label.x = 7.5, label.y = 46.5) +
+  stat_cor(label.x = 7.5, label.y = 43.5) 
+notree.trt.plot
+
+tiff("figures/2023-07_draft-figures/Corr-precip-veg.tiff", height = 5, width = 11, units = "in", res = 150)
+annotate_figure(ggarrange(notree.ctrl.plot, notree.trt.plot,
+                          nrow = 1, ncol = 2,
+                          labels = c("(A)", "(B)")),
+                top = text_grob("Correlation between precipitation and vegetation cover \n",
+                                size = 15,
+                                hjust = 1.1))
+dev.off()
 
 
 # Percent difference and elevation change
