6fccae0eb87be1d4c29c69558eaf8cfc625d25bc
Lia Ossanna <lossanna@arizona.edu>
2023-08-29 17:50:33 -0700

    Begin script for t-test code & figures

---
+++ scripts/Publish_t-test-2021.R
@@ -0,0 +1,9 @@
+# Purpose: Create 2021 figures for publication, and code for published R Markdown.
+#   Main figures:
+#   Supp figures: channel elevation change
+#   Code: t-test/Wilcox, plot
+
+# Created: 2023-08-28
+# Last updated: 2023-08-2
+
+library(tidyverse)
